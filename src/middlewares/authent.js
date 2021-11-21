cbe017d80fceb760cf9e2d1c1dccee77461e3cd7
Lonie <rbatoulime@gmail.com>
2021-11-21 09:21:08 +0000

    nothing

---
+++ src/middlewares/authent.js
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken"
+
+export default {
+
+    login: (request, response, next) => {
+        let {login, password} = request.body
+        
+    },
+
+    register: (request, response, next) => {
+        let {login, password} = request.body
+    }
+}
