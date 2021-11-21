1f868e2b1775481c6687f48a2c6318c534ac6a14
godwin <attipkoakrabal@gmail.com>
2021-11-21 00:29:46 +0000

    ajout de articles
    methode ajouter, chercherParType,chercherParid,supprimer,ajouterCommentaire

---
+++ src/models/article.js
@@ -0,0 +1,90 @@
+import pkg from '@prisma/client'
+import { request, response } from 'express'
+const {PrismaClient} = pkg
+const prisma = new PrismaClient()
+
+
+export default {
+
+    ajouter:(request,response) =>{
+        let {imageArticle} = request.files
+        let {titre ,descriptionArticle,liensArticle,typeArticle} = request.body
+        if(imageArticle&&titreArticle&&descriptionArticle&&liensArticle&&typeArticle){
+            let article = prisma.article.create({
+                data :{
+                    imageArticle: Buffer.from(imageArticle.data).toString('base64') ,
+                    titre: titre,
+                    descriptionArticle: descriptionArticle ,
+                    liensArticle: liensArticle ,
+                    idTypeArticle: typeArticle
+                }
+            })
+            return response.render()
+        }else{
+            return response.redirect()
+        }
+           
+    },
+
+    chercherParType:(request,response) =>{
+        let {typeArticle} = request.params
+        if(typeArticle){
+            let article = prisma.article.findMany({
+                where:{
+                    idTypeArticle: typeArticle
+                }
+            })
+            return response.render()
+        }else{
+            return response.redirect()
+        }
+    },
+
+    chercherParid:(request,response)=>{
+        let {idArticle} = request.params
+        if(idArticle){
+            let article = prisma.article.findUnique({
+                where:{
+                    idArticle:idArticle
+                }
+            })
+        }
+    },
+
+    supprimer:(request,response)=>{
+        let {idArticle} = request.params
+        if(idArticle){
+            let article = prisma.article.delete({
+                where:{
+                    idArticle:idArticle
+                }
+            })
+        }
+    },
+
+    ajouterCommentaire: (request,response)=>{
+        let {idArticle} = request.params
+        let {nomOuEmail,contenu} = request.body
+        if(idArticle){
+            let article = prisma.article.update({
+                where:{
+                    idArticle: idArticle
+                },
+                data:{
+                    commentaires:{
+                        create:{
+                            nomOuEmail: nomOuEmail,
+                            contenu: contenu
+                        }
+                    }
+                }
+            })       
+            return  response.render()
+        }
+
+        return response.redirect()
+    }
+
+
+
+}
