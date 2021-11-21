6bc7b3be7a2d8a72f534308e744516456220988a
Lonie <rbatoulime@gmail.com>
2021-11-21 21:54:07 +0000

    les middlewares de gestions d'erreur

---
+++ src/middlewares/handler.js
@@ -0,0 +1,36 @@
+
+export default {
+    userErrors: (request, response, next) => {
+        if(request.session.errors) {
+            response.locals.errors = request.session.errors
+            request.session.errors = undefined
+        }
+        request.errors = (content, redirect) => {
+    
+            if(request.session.errors == 'undefined') {
+                request.session.errors = {}
+            }
+            request.session.errors = content
+            response.redirect(redirect)
+        }
+
+        next()
+    },
+
+    systemErrorHandler: (request, response, next) => {
+        let err = new Error("Cette page est introuvable")
+        err.status = 404
+
+        next(err)
+    },
+
+    notFoundHandler: (err, request, response, next) => {
+        response.status(err.status || 5000)
+        response.send({
+            error: {
+                status: err.status || 500,
+                message: err.message
+            }
+        })
+    }
+}
